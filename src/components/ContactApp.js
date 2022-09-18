91a542a00cd6ffdc22ce1f17850ac176779ee67f
Rohman <rohmanpendo@gmail.com>
2022-09-18 12:20:20 +0700

    feat: change language using react context

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -6,6 +6,7 @@ import AddPage from '../pages/AddPage';
 import RegisterPage from '../pages/RegisterPage';
 import LoginPage from '../pages/LoginPage';
 import { getUserLogged, putAccessToken } from '../utils/api';
+import { LocaleProvider } from '../contexts/LocaleContext';
 
 class ContactApp extends React.Component {
     constructor(props) {
@@ -14,6 +15,21 @@ class ContactApp extends React.Component {
         this.state = {
             authUser: null,
             initializing: true,
+            localeContext: {
+                locale: localStorage.getItem('locale') || 'id',
+                toggleLocale: () => {
+                    this.setState((prevState) => {
+                        const newLocale = prevState.localeContext.locale === 'id' ? 'en' : 'id';
+                        localStorage.setItem('locale', newLocale);
+                        return {
+                            localeContext: {
+                                ...prevState.localeContext,
+                                locale: newLocale,
+                            }
+                        }
+                    });
+                }
+            }
         };
 
         this.onLoginSuccess = this.onLoginSuccess.bind(this);
@@ -43,33 +59,37 @@ class ContactApp extends React.Component {
         if (this.state.initializing) return null;
         if (!this.state.authUser) {
             return (
-                <div className="contact-app" >
-                    <header className="contact-app__header">
-                        <h1>Aplikasi Kontak</h1>
-                    </header>
-                    <main>
-                        <Routes>
-                            <Route path="/*" element={<LoginPage loginSuccess={this.onLoginSuccess} />} />
-                            <Route path="/register" element={<RegisterPage />} />
-                        </Routes>
-                    </main>
-                </div>
+                <LocaleProvider value={this.state.localeContext}>
+                    <div className="contact-app" >
+                        <header className="contact-app__header">
+                            <h1>Aplikasi Kontak</h1>
+                        </header>
+                        <main>
+                            <Routes>
+                                <Route path="/*" element={<LoginPage loginSuccess={this.onLoginSuccess} />} />
+                                <Route path="/register" element={<RegisterPage />} />
+                            </Routes>
+                        </main>
+                    </div>
+                </LocaleProvider>
             );
         }
 
         return (
-            <div className="contact-app" >
-                <header className="contact-app__header">
-                    <h1>Aplikasi Kontak</h1>
-                    <Navigation logout={this.onLogout} name={this.state.authUser.name} />
-                </header>
-                <main>
-                    <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/add" element={<AddPage />} />
-                    </Routes>
-                </main>
-            </div>
+            <LocaleProvider value={this.state.localeContext}>
+                <div className="contact-app" >
+                    <header className="contact-app__header">
+                        <h1>{this.state.localeContext.locale === 'id' ? 'Aplikasi Kontak' : 'Contacts App'}</h1>
+                        <Navigation logout={this.onLogout} name={this.state.authUser.name} />
+                    </header>
+                    <main>
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/add" element={<AddPage />} />
+                        </Routes>
+                    </main>
+                </div>
+            </LocaleProvider>
         );
     }
 }
